49a2cd9118812969fb6753f1f2f79b6bbc016884
YoungSooSong <thddudtn1@gmail.com>
2019-12-12 15:53:25 +0900

    오류수정

--- src/main/java/com/song/factory/SelectorFactory.java
+++ src/main/java/com/song/factory/SelectorFactory.java
@@ -3,6 +3,7 @@ package com.song.factory;
 import com.song.selector.MobileNaverSearchSelector;
 import com.song.selector.NaverSearchSelector;
 import com.song.selector.NaverShoppingSelector;
+import com.song.selector.NaverSmartStoreSelector;
 import com.song.selector.Selector;
 
 public class SelectorFactory extends Factory {
@@ -10,12 +11,14 @@ public class SelectorFactory extends Factory {
 	@Override
 	public Selector createSelector(String url) {
 		
-		if (url.contains("msearch.naver")) return new MobileNaverSearchSelector();
+		if (url.contains("msearch.shopping.naver")) return new MobileNaverSearchSelector();
 				
-		if (url.contains("search.naver")) return new NaverSearchSelector();
+		if (url.contains("search.shopping.naver")) return new NaverSearchSelector();
 		
 		//Mobile도 가능(Mobile URL접속 -> PC URL 자동 리다이렉트)
 		if (url.contains("shopping.naver")) return new NaverShoppingSelector();
+		//Mobile도 가능(Mobile URL접속 -> PC URL 자동 리다이렉트)
+		if (url.contains("smartstore.naver")) return new NaverSmartStoreSelector();
 		
 		return null;
 	}
