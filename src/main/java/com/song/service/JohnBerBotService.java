fefdc3e514a8869a70a3b9ffe5e42e15e0b6b6dd
YoungSooSong <thddudtn1@gmail.com>
2019-12-12 16:44:20 +0900

    가독성향상

--- src/main/java/com/song/service/JohnBerBotService.java
+++ src/main/java/com/song/service/JohnBerBotService.java
@@ -19,44 +19,43 @@ import com.song.crawler.NaverCrawler;
 import com.song.dao.JohnBerDao;
 import com.song.utils.CommonUtils;
 
-
 @Transactional
 @Service
 public class JohnBerBotService {
 	@Autowired
 	private JohnBerDao dao;
 	private NaverCrawler crawler = new NaverCrawler();
-	
+
 	public String getChatId(String chatId) throws Exception {
 		System.out.println("Service.getChatId");
 		return dao.getChatId(chatId);
 	}
-	
+
 	public int regUser(String chatId) throws Exception {
 		return dao.regUser(chatId);
 	}
-	
+
 	public int delUser(String chatId) throws Exception {
 		return dao.delUser(chatId);
-	}	
-	
+	}
+
 	public int regItem(Map<String, String> map) throws Exception {
 		return dao.regItem(map);
-	}	
-	
+	}
+
 	public int getPrice(HashMap<String, String> map) throws Exception {
 		return dao.getPrice(map);
 	}
-	
+
 	public List<HashMap<String, String>> getWishListAll() throws Exception {
 		return dao.getWishListAll();
 	}
-	
-	//가격변동 시 Update
+
+	// 가격변동 시 Update
 	public List<HashMap<String, String>> getUpdateList() throws Exception {
 		List<HashMap<String, String>> wishList = getWishListAll();
 		List<HashMap<String, String>> updateList = new ArrayList<HashMap<String, String>>();
-		
+
 		for (HashMap<String, String> map : wishList) {
 			String url = map.get("URL");
 			String oldPrice = map.get("ITEM_PRICE");
@@ -67,40 +66,37 @@ public class JohnBerBotService {
 			String newPrice = itemInfo.get("itemPrice").replace(",", "");
 			itemInfo.remove("itemPrice");
 			itemInfo.put("itemPrice", newPrice);
-			
+
 			if (oldPrice.equals(newPrice)) {
 				continue;
 			} else {
 				System.out.println("updateListAdded : " + itemInfo.toString());
 				updateList.add(itemInfo);
 			}
-			
 		}
 		return updateList;
 	}
 
-	//가격변동 시 Update 및 메시지 발송
+	// 가격변동 시 Update 및 메시지 발송
 	public int updatePrice(HashMap<String, String> map) throws Exception {
 		String itemPrice = map.get("itemPrice");
 		String oldPrice = map.get("oldPrice");
 		String itemName = map.get("itemName");
-		String message = "@@가격변동알림@@" + "\n상품명 : " + itemName +
-				"\n이전가격 : " + CommonUtils.moneyCommaUtil(oldPrice) + "\n현재가격 : " + CommonUtils.moneyCommaUtil(itemPrice);
+		String message = "@@가격변동알림@@" + "\n상품명 : " + itemName + "\n이전가격 : " + CommonUtils.moneyCommaUtil(oldPrice)
+				+ "\n현재가격 : " + CommonUtils.moneyCommaUtil(itemPrice);
 		sendMessage(message, map.get("chatId"));
 		return dao.updatePrice(map);
 	}
-	
-    public String sendMessage(String message, String chatId) {
-    	TelegramBot bot = new TelegramBot("952633662:AAGDJOld3g9M691pvvMM8ULmF7oRzYhkvR4"); 
-    	SendMessage request = new SendMessage(chatId, message)
-    	        .parseMode(ParseMode.HTML)
-    	        .disableWebPagePreview(true)
-    	        .disableNotification(false);
 
-    	SendResponse sendResponse = bot.execute(request);
-    	boolean ok = sendResponse.isOk();
-    	Message responseMessage = sendResponse.message();
-    	//System.out.println( "responseMessage : " + responseMessage);
-    	return String.valueOf(ok);
-    }
+	public String sendMessage(String message, String chatId) {
+		TelegramBot bot = new TelegramBot("952633662:AAGDJOld3g9M691pvvMM8ULmF7oRzYhkvR4");
+		SendMessage request = new SendMessage(chatId, message).parseMode(ParseMode.HTML).disableWebPagePreview(true)
+				.disableNotification(false);
+
+		SendResponse sendResponse = bot.execute(request);
+		boolean ok = sendResponse.isOk();
+		Message responseMessage = sendResponse.message();
+		// System.out.println( "responseMessage : " + responseMessage);
+		return String.valueOf(ok);
+	}
 }
