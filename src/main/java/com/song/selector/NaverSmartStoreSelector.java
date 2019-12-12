2feade7469bedb90067f8ea51cef94abc984fcce
YoungSooSong <thddudtn1@gmail.com>
2019-12-12 16:44:34 +0900

    스마트스토어 Selector 추가

---
+++ src/main/java/com/song/selector/NaverSmartStoreSelector.java
@@ -0,0 +1,26 @@
+package com.song.selector;
+
+public class NaverSmartStoreSelector implements Selector{
+	
+	private String priceSelector = "#wrap > div > div.prd_detail_basic > div.info > form > fieldset > div._copyable > dl > dd > div.area_cost > strong > span.thm";
+	private String itemNameSelector = "#wrap > div > div.prd_detail_basic > div.info > form > fieldset > div._copyable > dl > dt > strong";
+	
+	public String getPriceSelector() {
+		return priceSelector;
+	}
+	public void setPriceSelector(String priceSelector) {
+		this.priceSelector = priceSelector;
+	}
+	public String getItemNameSelector() {
+		return itemNameSelector;
+	}
+	public void setItemNameSelector(String itemNameSelector) {
+		this.itemNameSelector = itemNameSelector;
+	}
+	
+	@Override
+	public String toString() {
+		return "NaverShoppingSelector [priceSelector=" + priceSelector + ", itemNameSelector=" + itemNameSelector + "]";
+	}
+	
+}
