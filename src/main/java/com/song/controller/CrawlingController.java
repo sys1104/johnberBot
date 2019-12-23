66ee99585310e6d605f450faff92a84e04f22bb1
YoungSooSong <thddudtn1@gmail.com>
2019-12-23 14:51:43 +0900

    미사용메소드 제거, cron 스케줄러 추가, Map 자료형 변경

--- src/main/java/com/song/controller/CrawlingController.java
+++ src/main/java/com/song/controller/CrawlingController.java
@@ -7,9 +7,10 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,22 +26,6 @@ public class CrawlingController {
 	@Autowired
 	JohnBerBotService service;
 	Logger logger = LoggerFactory.getLogger("com.song.controller.CrawlingController");
-	@RequestMapping("/price")
-	public ModelAndView getPrice() {
-		
-		try {
-			logger.debug(service.getChatId("77000118"));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		ModelAndView mav = new ModelAndView("Result");
-		Map<String, String> map = new HashMap<>();
-		
-		map.put("price", "500");
-		mav.addObject("map", map);
-		return mav;
-	}
 	
 	@RequestMapping("/getChatId")
 	public String getChatId(HttpServletRequest request) {
@@ -79,8 +64,7 @@ public class CrawlingController {
 		}
 	}
 	@RequestMapping("/regItem")
-	public void regItem(@RequestParam Map<String, String> paramMap) {
-		
+	public void regItem(@RequestParam HashMap<String, String> paramMap) {
 		int result = -1;
 		try {
 			result = service.regItem(paramMap);
@@ -91,9 +75,9 @@ public class CrawlingController {
 		}
 	}
 	@RequestMapping("/getWishListAll")
-	public List<HashMap<String, String>> getWishListAll() {
+	public List<HashMap<String, Object>> getWishListAll() {
 		
-		List<HashMap<String, String>> itemList = new ArrayList<HashMap<String, String>>();
+		List<HashMap<String, Object>> itemList = new ArrayList<HashMap<String, Object>>();
 		try {
 			itemList = service.getWishListAll();
 			logger.debug(itemList.size() +  "건 " );
@@ -105,17 +89,19 @@ public class CrawlingController {
 	} 
 	
 	@RequestMapping("/updatePrice")
+	//매 시간 12분마다
+	@Scheduled(cron = "0 12 * * * *")
 	public void updatePrice() {
 		
-		List<HashMap<String, String>> updateList = new ArrayList<HashMap<String, String>>();
+		List<HashMap<String, Object>> updateList = new ArrayList<HashMap<String, Object>>();
 		try {
 			updateList = service.getUpdateList();
 			logger.debug( "업데이트 대상 리스트 : " + updateList.size() +  "건 " );
 			if (updateList.size() > 0) {
-				for (HashMap<String, String> map : updateList) {
+				for (HashMap<String, Object> map : updateList) {
 					logger.debug(service.updatePrice(map) + "건 업데이트 완료");
 				}
-			}			
+			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
