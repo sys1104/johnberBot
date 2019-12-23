af3085115bd811576fd5f14ef1fec38f2fe719bf
YoungSooSong <thddudtn1@gmail.com>
2019-12-23 14:36:41 +0900

    스케줄러 어노테이션 추가

--- src/main/java/com/song/SpringWebServiceApplication.java
+++ src/main/java/com/song/SpringWebServiceApplication.java
@@ -3,6 +3,7 @@ package com.song;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
@@ -13,6 +14,7 @@ import com.song.controller.MyAmazingBot;
 //@MapperScan(value={"com.song.dao"})
 @SpringBootApplication
 @ComponentScan
+@EnableScheduling
 public class SpringWebServiceApplication {
 	
 	public static void main(String[] args) {
