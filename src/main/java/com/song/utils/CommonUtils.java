8cf59dff630d71dfa108cb0af3d928c8d8a9a8f4
YoungSooSong <thddudtn1@gmail.com>
2020-01-31 11:46:12 +0900

    1. rawUrlToATag 메소드 추가

--- src/main/java/com/song/utils/CommonUtils.java
+++ src/main/java/com/song/utils/CommonUtils.java
@@ -11,5 +11,14 @@ public final class CommonUtils {
 	public static String removeComma (String str) {
 		str = str.replace(",", "");
 		return str;
-	}	
+	}
+	
+	public static String rawUrlToATag (String url) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<a href=\"");
+		sb.append(url);
+		sb.append("\">이동</a>");
+		return sb.toString();
+		
+	}
 }
